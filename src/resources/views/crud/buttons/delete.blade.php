d0b4ac889aac0d70ba273e07ac554278bdfd8caf
Antonio Almeida <promatik@gmail.com>
2020-11-11 23:54:14 +0000

    Added title to line buttons

--- src/resources/views/crud/buttons/delete.blade.php
+++ src/resources/views/crud/buttons/delete.blade.php
@@ -1,5 +1,8 @@
 @if ($crud->hasAccess('delete'))
-	<a href="javascript:void(0)" onclick="deleteEntry(this)" data-route="{{ url($crud->route.'/'.$entry->getKey()) }}" class="btn btn-sm btn-link" data-button-type="delete"><i class="la la-trash"></i> <span>{{ trans('backpack::crud.delete') }}</span></a>
+	<a href="javascript:void(0)" onclick="deleteEntry(this)" data-route="{{ url($crud->route.'/'.$entry->getKey()) }}" class="btn btn-sm btn-link" data-button-type="delete" title="{{ trans('backpack::crud.delete') }}">
+		<i class="la la-trash"></i>
+		<span>{{ trans('backpack::crud.delete') }}</span>
+	</a>
 @endif
 
 {{-- Button Javascript --}}
--- src/resources/views/crud/buttons/update.blade.php
+++ src/resources/views/crud/buttons/update.blade.php
@@ -1,12 +1,13 @@
 @if ($crud->hasAccess('update'))
 	@if (!$crud->model->translationEnabled())
-
-	<!-- Single edit button -->
-	<a href="{{ url($crud->route.'/'.$entry->getKey().'/edit') }}" class="btn btn-sm btn-link"><i class="la la-edit"></i> <span>{{ trans('backpack::crud.edit') }}</span></a>
-
+	{{-- Single edit button --}}
+	<a href="{{ url($crud->route.'/'.$entry->getKey().'/edit') }}" class="btn btn-sm btn-link" title="{{ trans('backpack::crud.edit') }}">
+		<i class="la la-edit"></i>
+		<span>{{ trans('backpack::crud.edit') }}</span>
+	</a>
 	@else
 
-	<!-- Edit button group -->
+	{{-- Edit button group --}}
 	<div class="btn-group">
 	  <a href="{{ url($crud->route.'/'.$entry->getKey().'/edit') }}" class="btn btn-sm btn-link pr-0"><i class="la la-edit"></i> <span>{{ trans('backpack::crud.edit') }}</span></a>
 	  <a class="btn btn-sm btn-link dropdown-toggle text-primary pl-1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
